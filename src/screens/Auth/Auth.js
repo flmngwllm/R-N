aeec0a03344feeeb66a3adb13c1eb8d19605c448
Will <flmngwllm232@gmail.com>
2019-01-15 16:06:29 -0500

    added navigation

---
+++ src/screens/Auth/Auth.js
@@ -0,0 +1,15 @@
+import React, {component} from 'react'
+import {View, Text} from 'react-native'
+
+class AuthScreen extends Component{
+    render(){
+        return(
+
+            <View>
+                <Text>AuthScreen</Text>
+            </View>
+        )
+    }
+}
+
+export default AuthScreen
